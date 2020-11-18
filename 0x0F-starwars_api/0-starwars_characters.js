ae68958086b5705ade72ae58af746c13e0a0c8c8
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-17 19:36:58 -0500

    [Fix] Do how the vefore exercise

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -2,16 +2,22 @@
 /*
   Who was playing in this movie?
 */
-const request = require('request');
-const meurl = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2] + '/';
 
-request(meurl, (error, response, body) => {
-  const res = JSON.parse(body).characters;
-  for (const urlchar of res) {
-    request(urlchar, (error, response, body) => {
-      const character = JSON.parse(body);
-      console.log(character.name);
-    });
+const request = require('request');
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body).characters;
+    for (let i = 0; i < data.length; i++) {
+      request(data[i], function (error, response, body) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
   }
 });
-
