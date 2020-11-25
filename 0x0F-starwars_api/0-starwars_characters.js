78e43c1f45309b03870dd18ad1f8e3926c7c28fc
Santiago Yanguas <santitoya-2001@outlook.com>
2020-11-24 20:07:51 -0500

    [Fix] Change the characters

--- 0x0F-starwars_api/0-starwars_characters.js
+++ 0x0F-starwars_api/0-starwars_characters.js
@@ -3,21 +3,36 @@
   Who was playing in this movie?
 */
 
+const argv = process.argv;
+const urlMovie = `https://swapi-api.hbtn.io/api/films/${argv[2]}/`;
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    const data = JSON.parse(body).characters;
-    for (let i = 0; i < data.length; i++) {
-      request(data[i], function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
+
+request(urlMovie, function (error, response, body) {
+  if (error == null) {
+    const mebody = JSON.parse(body);
+    const characters = mebody.characters;
+
+    if (characters && characters.length > 0) {
+      const final = characters.length;
+      ReqString(0, characters[0], characters, final);
     }
+  } else {
+    console.log(error);
   }
 });
+
+function ReqString(index, url, characters, final) {
+  if (index === final) {
+    return;
+  }
+  request(url, function (error, response, body) {
+    if (!error) {
+      const mebody = JSON.parse(body);
+      console.log(mebody.name);
+      index++;
+      ReqString(indx, characters[idx], characters, final);
+    } else {
+      console.error('error:', error);
+    }
+  });
+}
